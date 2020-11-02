c39de609d3a23394a9902b6cdb33060279fc984c
SethMacP <macpherson.seth@gmail.com>
2020-11-02 14:51:55 -0800

    MVP Met

---
+++ team-builder/src/data.js
@@ -0,0 +1,30 @@
+export default [
+    {
+        id      :   1,
+        name    :   "Htes NosrehPcam",
+        email   :   "Nunya@business.com",
+        role    :   "Da Boss",
+        eyes    :   "brown"
+    },
+    {
+        id      :   2,
+        name    :   "Htes NosrehPcam",
+        email   :   "Nunya@business.com",
+        role    :   "Da Boss",
+        eyes    :   "brown"
+    },
+    {
+        id      :   3,
+        name    :   "June Bug",
+        email   :   "bigeyes@imabug.com",
+        role    :   "Da Eyes",
+        eyes    :   "Too many"
+    },
+    {
+        id      :   4,
+        name    :   "Killer Kate",
+        email   :   "scary@dontmesswithme.com",
+        role    :   "Da Scariest",
+        eyes    :   "black"
+    }
+]
